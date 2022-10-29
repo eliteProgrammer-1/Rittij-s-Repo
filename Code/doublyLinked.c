27c94384df069ba5c5ea67802ef4b5c4add83b19
Manish Sharma <sharmamanish16111999@gmail.com>
2022-10-29 18:01:49 +0530

    commit-3

--- Code/doublyLinked.c
+++ Code/doublyLinked.c
@@ -1,35 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<stdlib.h>
 
 struct node
 {
     int data;
-    struct node* prev;
-    struct node* next;
+    struct node *prev;
+    struct node *next;
 };
 
-
-struct node* createNode()
+struct node *createNode(int val)
 {
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    newNode->data = val;
+    
     return newNode;
 }
 
-void addNode(struct node* head, int val)
+struct node* addNodeAtHead(struct node* head, int val)
 {
-   
-    struct node* newNode = createNode();
+    struct node* newNode = createNode(val);
+    newNode->next = head;
+    head->prev = newNode;
+    return newNode; // new head of given linkedList
+}
+
+void addNodeAtTail(struct node *head, int val)
+{
+
+    struct node *newNode = createNode(val);
     // (*newNode).data = val;
-    newNode->data = val;
-    newNode->next = NULL;
     while (head->next != NULL)
-    {
+    {   
+        // printf("%d ", head->data);
         head = head->next;
     }
     newNode->prev = head;
     head->next = newNode;
 }
 
-void forwardTravsersal(struct node* head)
+void forwardTravsersal(struct node *head)
 {
     while (head != NULL)
     {
@@ -38,13 +50,68 @@ void forwardTravsersal(struct node* head)
     }
 }
 
-void backwardTraversal(struct node *tail)
+struct node *getTail(struct node *head)
 {
-
+    struct node *tail;
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+    tail = head;
+    return tail;
 }
 
+void backwardTraversal(struct node *head)
+{
+    struct node *tail = getTail(head);
+    while (tail != NULL)
+    {
+        printf("%d ", tail->data);
+        tail = tail->prev;
+    }
+}
+
+struct node* addNodeAtMid(struct node* head, int index, int val)
+{   
+    struct node* newHead = head;
+    struct node* newNode = createNode(val);
+    if (index == 0)
+    {
+        // insertion at head
+    }
+    else
+    {
+        int currIndex = 0;
+        while (currIndex < index - 1)
+        {   
+            head = head->next;
+            currIndex++;
+        }
+        newNode->next = head->next;
+        head->next->prev = newNode;
+
+        head->next = newNode;
+        newNode->prev = head;
+    }
+    return newHead;
+}
 
 int main()
 {
+    struct node* head = (struct node*)malloc(sizeof(struct node));
+    head->data = 2;
+    head->prev = NULL;
+    head->next = NULL;
 
+    addNodeAtTail(head, 5);
+    addNodeAtTail(head, 6);
+    addNodeAtTail(head, 7);
+
+    // head = addNodeAtHead(head, 10);
+    // head = addNodeAtHead(head, 12);
+
+    head = addNodeAtMid(head, 2, 100);
+
+    forwardTravsersal(head);
+    printf("\n");
 }
